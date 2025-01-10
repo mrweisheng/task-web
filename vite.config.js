cb4368d8bbc5b273f33d35d5aa5b2d72b152182c
mrweisheng <mrshengwei@gmail.com>
2025-01-10 16:01:44 +0800

    fix build render

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const isRender = process.env.RENDER === 'true'
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/task-web/',
+  base: isRender ? '/task-web/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
